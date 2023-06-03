2b1c21ff9bb6a2ce986c28979793b2bd61d167ca
Svetoslav Savov <101751315+Svetoslav534361434@users.noreply.github.com>
2023-06-03 23:44:36 +0300

    Add files via upload

--- Superman.java
+++ Superman.java
@@ -1,25 +1,62 @@
-
+package superman;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Superman {
 
 	public static void main(String[] args) {
-		Superman1 s1 = new Superman1("Петър", "герой");
-		Superman1 s2 = new Superman1("Стивън", "злодей");
-		//Superman1 s3 = new Superman1("");
-		System.out.println("");
-		System.out.println("");
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        ArrayList <String> neshto = new ArrayList <String>();
 		
-		s1.print();
+		neshto.add("nametalo");
+		for(int c = 0; c <= neshto.size(); c++) {
+			System.out.print(neshto.get(c));
+		}
+        
+		Superman1 s1 = new Superman1("Петър", "добър");
+		Superman1 s2 = new Superman1("Ангел", "зъл");
+		Superman1 s3 = new Superman1("Стивън");
+		//System.out.println(s1.ime + "; " + s1.strana);
+		//System.out.println(s2.ime + "; " + s2.strana);
+		//System.out.println(s3.ime);
+		
+		int s1Godini = s1.getGodini();
+		System.out.println(s1Godini);
+		s1.setGodini(30);
+		
 		s1.leti();
-//		s2.bieSe();
+		s2.bieSe();
+
+		int[][] map = {
+				{1, 1, 1} ,
+				{2, 3, 3} ,
+				{4, 1, 4} ,
+				{3, 2, 1}
+		};		
+		for(int i = 0; i<=map.length; i++) {
+			int[] arr = map[i];
+			for(int j = 0; j<=arr.length; j++) {
+				if (arr[j] == 1) {
+					System.out.println("🌍");
+				} else if (arr[j] == 2) {
+					System.out.println('☀');
+				} else if (arr[j] == 3) {
+					System.out.println("🌙");
+				} else if (arr[j] == 4) {
+					System.out.println('⚠');
+				}	
+			}
+		}
 		
-		int[][] map =  {
-				{1,1,1},
-				{2,3,3},
-				{4,1,4},
-				{3,2,1},
-		};
-		//for(int )
-        //
+		//Избор на герой
+		if(x==1) {
+		System.out.println("Izbrahte geroy ot stranata na dobroto " + s1.getIme() + s1.getStrana());	
+		s1.leti();
+		}
+		else {
+		System.out.println("Izbrahte geroy ot stranata na zloto" + s2.getIme() + s2.getStrana());	
+		s2.bieSe();
+		}
 	}
 
 }
--- Superman1.java
+++ Superman1.java
@@ -1,52 +1,71 @@
+package superman;
 
 public class Superman1 {
-    private String ime;
-    private String strana;
-    
-    public Superman1() {
-    	
-    }
-    public Superman1(String i) {
-    	ime =i;
-    }
-    public Superman1(String i, String s) {
-    	ime = i;
-    	strana = s;
-    }
-    public void print() {
-    	System.out.println("Аз съм " + ime + " и съм " + strana);
-    }
-    
-    public void leti() {
-    	System.out.println("         ,");
-		System.out.println("       0/");
-		System.out.println("      /7,");
-		System.out.println("    .'(");
-		System.out.println("  '^ / >");
-		System.out.println("    / <");
-		System.out.println("    `");
-    }
-//  public void bieSe() {
-//  	String k = """
-//   	    .=.,
-//            ;c =\
-//        __|  _/
-//      .'-'-._/-'-._
-//       /..   ____    \
-//      /' _  [<_->] )  \
-//     (  / \--\_>/-/'._ )
-//      \-;_/\__;__/ _/ _/
-//     '._}|==o==\{_\/
-//        /  /-._.--\  \_
-//       // /   /|   \ \ \
-//      / | |   | \;  |  \ \
-//    / /  | :/   \: \   \_\
-//    /  |  /.'|   /: |    \ \
-//    |  |  |--| . |--|     \_\
-//    / _/   \ | : | /___--._) \
-//   |_(---'-| >-'-| |       '-'
-//          /_/     \_\
-//    	""";
-//    	System.out.println(k);
-//   }
+private String ime;
+private String strana;
+private int godini;
+
+public Superman1() {
+	
+}
+public Superman1(String i) {
+	ime = i;
+}
+public String getIme() {
+	return ime;
+}
+public String getStrana() {
+	return strana;
+}
+public int getGodini() {
+	return godini;
+}
+public void setGodini(int g) {
+	if (g >= 0 && g <=100) {
+	godini = g;
+	}
+}
+
+public Superman1(String i, String s) {
+	ime = i;
+	strana = s;
+}
+
+public void leti() {
+	System.out.println("        ,");
+	System.out.println("      0/");
+	System.out.println("     /7,");
+	System.out.println("   .'(");
+	System.out.println(" '^ / >");
+	System.out.println("   / <");
+	System.out.println("   `");
+	
+}
+public void bieSe() {
+	System.out.println("         .=.,");
+	System.out.println("        ;c =\\ ");
+	System.out.println("       __|  _/");
+	System.out.println("     .'-'-._/-'-._ ");
+	System.out.println("    /..   __    \\ ");
+	System.out.println("   /' _  [<_->] )  \\ ");
+	System.out.println("  (  / \\--\\_>/-/'._ ) ");
+	System.out.println("   \\-;_/\\__;__/ _/ _/ ");
+	System.out.println("    '._}|==o==\\{_\\/ ");
+	System.out.println("     /  /-._.--\\  \\_");
+	System.out.println("    // /   /|   \\ \\ \\");
+	System.out.println("   / | |   | \\;  |  \\ \\");
+	System.out.println("  / /  |  |   \\: \\   \\_\\");
+	System.out.println(" /  |  /.'|   /: |    \\ \\ ");
+	System.out.println(" |  |  |--| . |--|     \\_\\");
+	System.out.println(" / /   \\ | : | /___--.) \\");
+	System.out.println("|_(---'-| >-'-| |       '-'");
+	System.out.println("snd    /_/     \\_\\ ");
+	
+}
+
+public void print() {
+	System.out.println("Аз съм " + ime + " и съм " + strana);
+	System.out.println("На " + godini + "години съм");
+	//System.out.println("Аз съм " + ime + " и съм " + strana);
+}
 }
